ae1fb59465a52e796f37a6588e9162e74c0feeac
blakawk <blakawk@gentooist.com>
2011-05-02 17:10:11 +0200

    Updated Server
    * Defined sub-classes for Connection and ConnectionFactory implementations
    * Updated the test to call enableWriteInterest() one last time
    
    Closes: #84
    Testing: 100%

--- test/Server.java
+++ test/Server.java
@@ -22,8 +22,6 @@
  * <http://www.gnu.org/licenses/>.
  */
 import com.aio4c.Aio4c;
-import com.aio4c.Connection;
-import com.aio4c.ConnectionFactory;
 import com.aio4c.ServerConfig;
 import com.aio4c.buffer.Buffer;
 import com.aio4c.buffer.BufferOverflowException;
@@ -31,52 +29,56 @@ import com.aio4c.log.Log;
 import com.aio4c.log.DefaultLogger;
 
 public class Server {
+    private static class Connection extends com.aio4c.Connection {
+        private boolean first = true;
+        @Override
+        public void onInit() {
+            Log.debug("["+this+"] initialized");
+        }
+        @Override
+        public void onConnect() {
+            Log.debug("["+this+"] connected");
+            enableWriteInterest();
+        }
+        @Override
+        public void onClose() {
+            Log.debug("["+this+"] closed");
+        }
+        @Override
+        public void onRead(Buffer data) {
+            String s = data.getString();
+            Log.debug("["+this+"] received '"+s+"', buffer "+data);
+            if (first) {
+                enableWriteInterest();
+                first = false;
+            }
+        }
+        @Override
+        public void onWrite(Buffer data) {
+            try {
+                data.putString("BONJOUR");
+                data.putString("QUIT");
+                Log.debug("["+this+"] sending "+data);
+            } catch (BufferOverflowException e) {
+                e.printStackTrace();
+                close(true);
+            }
+        }
+    }
+
+    private static class ConnectionFactory implements com.aio4c.ConnectionFactory {
+        @Override
+        public Connection create() {
+            return new Connection();
+        }
+    }
+
     public static void main(String[] args) {
         Aio4c.init(args, new DefaultLogger());
         Thread t = new Thread() {
             @Override
             public void run() {
-                final com.aio4c.Server server = new com.aio4c.Server(new ServerConfig(), new ConnectionFactory() {
-                    @Override
-                    public Connection create () {
-                        return new Connection () {
-                            private boolean first = true;
-                            @Override
-                            public void onInit() {
-                                Log.debug("["+this+"] initialized");
-                            }
-                            @Override
-                            public void onConnect() {
-                                Log.debug("["+this+"] connected");
-                                enableWriteInterest();
-                            }
-                            @Override
-                            public void onClose() {
-                                Log.debug("["+this+"] closed");
-                            }
-                            @Override
-                            public void onRead(Buffer data) {
-                                String s = data.getString();
-                                Log.debug("["+this+"] received '"+s+"', buffer "+data);
-                                if (first) {
-                                    enableWriteInterest();
-                                    first = false;
-                                }
-                            }
-                            @Override
-                            public void onWrite(Buffer data) {
-                                try {
-                                    data.putString("BONJOUR");
-                                    data.putString("QUIT");
-                                    Log.debug("["+this+"] sending "+data);
-                                } catch (BufferOverflowException e) {
-                                    e.printStackTrace();
-                                    close(true);
-                                }
-                            }
-                        };
-                    }
-                });
+                final com.aio4c.Server server = new com.aio4c.Server(new ServerConfig(), new ConnectionFactory());
                 server.start();
                 Runtime.getRuntime().addShutdownHook(new Thread() {
                     public void run() {
